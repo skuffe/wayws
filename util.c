cc21bcf72d5c2ffc0bc4f0acd781c8c6edda7103
Danni Skov Høglund <skuffe@pwnz.dk>
2025-07-23 02:30:55 +0200

    refactor: remove unused debug functions from util.c
    - Remove dbg_append, dbg_append_int, and dbg_append_hexptr functions
    - Remove unused #include <stdint.h>
    - All tests pass, no functionality lost
    - Reduces code size and complexity

--- util.c
+++ util.c
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdint.h>
 
 void *xrealloc(void *p, size_t n) {
   void *q = realloc(p, n);
@@ -35,41 +34,4 @@ int isnum(const char *s) {
 void die(const char *msg) {
   fputs(msg, stderr);
   exit(1);
-}
-
-void dbg_append(char *buf, size_t *len, size_t cap, const char *s) {
-  while (*s && *len + 1 < cap)
-    buf[(*len)++] = *s++;
-}
-void dbg_append_int(char *buf, size_t *len, size_t cap, int v) {
-  char tmp[32];
-  int tlen = 0;
-  if (v == 0)
-    tmp[tlen++] = '0';
-  else {
-    unsigned int u = (v < 0) ? (unsigned int)(-v) : (unsigned int)v;
-    char rev[32];
-    int rlen = 0;
-    while (u) {
-      rev[rlen++] = (char)('0' + (u % 10));
-      u /= 10;
-    }
-    if (v < 0)
-      tmp[tlen++] = '-';
-    while (rlen--)
-      tmp[tlen++] = rev[rlen];
-  }
-  for (int i = 0; i < tlen && *len + 1 < cap; ++i)
-    buf[(*len)++] = tmp[i];
-}
-void dbg_append_hexptr(char *buf, size_t *len, size_t cap, uintptr_t p) {
-  dbg_append(buf, len, cap, "0x");
-  char hex[2 * sizeof p];
-  int hlen = 0;
-  for (int i = (int)(sizeof p * 2) - 1; i >= 0; --i) {
-    int nib = (int)((p >> (i * 4)) & 0xF);
-    hex[hlen++] = (char)(nib < 10 ? '0' + nib : 'a' + nib - 10);
-  }
-  for (int i = 0; i < hlen && *len + 1 < cap; ++i)
-    buf[(*len)++] = hex[i];
 }
